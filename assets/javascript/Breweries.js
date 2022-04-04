6d34ff0297b2a00d58a0a2f082ffe2c7b6bbe1ea
Sensess <SensessYentzer@outlook.com>
2022-04-04 19:27:44 -0400

    Breweries js

---
+++ assets/javascript/Breweries.js
@@ -0,0 +1,34 @@
+const breweryList = document.querySelector(".brewery-list");
+const breweryName = document.querySelector(".brewery-name");
+function getBrewery(name) {
+  console.log(name);
+  name = name.toString();
+  fetch(
+    'https://api.openbrewerydb.org/breweries?by_city=${name}')    
+    
+    .then((response) => {
+      response.json().then((data) => {
+        const brewery = data.brewery;
+        console.log(brewery);
+
+        if (brewery.length === 0) {
+          const breweryWarning = document.createElement("h3");
+          breweryWarning.textContent = "Sorry, no breweries in that city!";
+          breweryList.appendChild(breweryWarning);
+          return;
+        }
+        brewery.forEach((e) => {
+          const breweryItem = document.createElement('li');
+          const breweryLink = document.createElement('a');
+          breweryLink.setAttribute('href', el.website_url);
+          breweryLink.setAttribute('class', 'event-block');
+          breweryItem.textContent = el.name;
+          breweryLink.appendChild(breweryItem);
+          breweryList.appendChild(breweryLink);
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
