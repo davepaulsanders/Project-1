5222dfac12f68bae8ab55e1416bb85e10263fc95
Sensess <SensessYentzer@outlook.com>
2022-04-05 17:17:12 -0400

    redo pt 30000000

--- assets/javascript/Breweries.js
+++ assets/javascript/Breweries.js
@@ -1,33 +1,27 @@
 const breweryList = document.querySelector(".brewery-list");
 const breweryName = document.querySelector(".brewery-name");
-function getBrewery(name) {
-  console.log(name);
-  fetch(
-    `https://api.openbrewerydb.org/breweries?by_name=${encodeURIComponent(name)}`)    
-    
-    .then((response) => {
-      response.json().then((data) => {
-        const brewery = data.brewery;
-        console.log(data);
-
-        if (data.length === 0) {
-          const breweryWarning = document.createElement("h3");
-          breweryWarning.textContent = "Sorry, no breweries in that city!";
-          breweryList.appendChild(breweryWarning);
-          return;
+        async function listBreweries(city) {
+            try {
+                const response = await fetch(`https://api.openbrewerydb.org/breweries?by_city=${encodeURIComponent(city)}`);
+                const breweries = await response.json()
+                console.table(breweries);
+                if (breweries.length === 0) {
+                    const breweryWarning = document.createElement('h3');
+                    breweryWarning.textContent = 'Sorry, no breweries in that city!';
+                    breweriesList.appendChild(breweryWarning);
+                    return;
+                }
+                breweries.forEach(brewery => {
+                    const breweryLink = document.createElement('a');
+                    breweryLink.setAttribute('href', brewery.website_url);
+                    breweryLink.setAttribute('class', 'event-block');
+                    breweryLink.textContent = brewery.name;
+                    const breweryItem = document.createElement('li');
+                    breweryItem.appendChild(breweryLink);
+                    breweryList.appendChild(breweryItem);
+                });
+            } catch (error) {
+                console.error(error);
+            };
         }
-        brewery.forEach((e) => {
-          const breweryItem = document.createElement('li');
-          const breweryLink = document.createElement('a');
-          breweryLink.setAttribute('href', el.website_url);
-          breweryLink.setAttribute('class', 'event-block');
-          breweryItem.textContent = el.name;
-          breweryLink.appendChild(breweryItem);
-          breweryList.appendChild(breweryLink);
-        });
-      });
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-}
+        
